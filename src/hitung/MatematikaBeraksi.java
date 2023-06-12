8348cca6c6a3b658df2d7fcbef776136d40edf42
teddykiswanto <teddykiswanto17@gmail.com>
2023-06-12 21:09:32 +0800

    Membuat kelas MatekamtikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hitung;
 
-/**
- *
- * @author 62831
- */
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        // membuat objek
+        Matematika teddy = new Matematika(5,0);
+        System.out.println("Hasil Penjumlahan: "+teddy.setPenjumlahan());
+        System.out.println("Hasil Pengurangan: "+teddy.setPengurangan());
+        System.out.println("Hasil Perkalian: "+teddy.setPerkalian());
+        System.out.println("Hasil Pembagian: "+teddy.setPembagian());
+    }
 }
